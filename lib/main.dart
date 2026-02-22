a0df3826c86bf29a8929137c4d8579f75f92596a
Tarik Khazma <taarekkhazmaa@gmail.com>
2026-02-22 07:08:02 +0300

    Fix unused import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
-// import 'package:fintech_app/core/constants/app_theme.dart';
-// import 'package:fintech_app/features/view/screens/onboarding/onboarding_one.dart';
-import 'package:fintech_app/features/view/auth/create_account_screen/create_account_screen.dart';
-// import 'package:fintech_app/core/constants/app_theme.dart';
-// import 'package:fintech_app/features/view/auth/create_account_screen_page_two/create_account_screen_page_two.dart';
 import 'package:fintech_app/features/view/onborading/screens/onboarding_main.dart';
-// import 'package:fintech_app/features/view/screens/onboarding/onboarding_two.dart';
-// import 'package:fintech_app/features/view/screens/splash/splash_screens.dart';
 import 'package:flutter/material.dart';
 
 void main() {
